fe370d794323a34f7ec56b4def9b5ca8931765b5
sunnz <sunnzy@pm.me>
2021-07-18 17:04:58 +1000

    added tags-to-orgs map for direct lookup

---
+++ src/make-tags-org-map.test.js
@@ -0,0 +1,33 @@
+const makeTagsOrgMap = require("./make-tags-org-map");
+const orgs = [
+  {
+    _id: 101,
+    name: "Mental Health First Aid",
+    tags: ["Fulton", "West", "Rodriguez", "Farley"],
+  },
+  {
+    _id: 102,
+    name: "Nutralab",
+    tags: ["Cherry", "Collier", "Fuentes", "Farley"],
+  },
+];
+
+test("given a tag cherry the map returns 102 by direct lookup", () => {
+  const map = makeTagsOrgMap(orgs);
+  expect(map).toBeInstanceOf(Map);
+
+  const ids = map.get("cherry");
+  expect(ids).toBeInstanceOf(Set);
+  expect(ids).not.toContain(101);
+  expect(ids).toContain(102);
+});
+
+test("given a tag farley the map returns both 101 and 102 by direct lookup", () => {
+  const map = makeTagsOrgMap(orgs);
+  expect(map).toBeInstanceOf(Map);
+
+  const ids = map.get("farley");
+  expect(ids).toBeInstanceOf(Set);
+  expect(ids).toContain(101);
+  expect(ids).toContain(102);
+});
